c14210349ef78f472998d9d027c1e2b13017a15d
Ng Reeve <20025415@myrp.edu.sg>
2022-08-05 09:50:41 +0800

    Registration

---
+++ C206_CaseStudy/src/Registration.java
@@ -0,0 +1,18 @@
+
+public class Registration {
+	public int mobileNumber;
+	public String name;
+	
+	public Registration(int mobileNumber, String name) {
+		this.mobileNumber = mobileNumber;
+		this.name = name;
+	}
+	
+	public void setName(String name) {
+		this.name = name; 
+	}
+	
+	public void setMobileNumber(int mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+}
